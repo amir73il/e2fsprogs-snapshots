ee1d57878b8717b8e0477bbe7337cc720866b993
Amir Goldstein <amir@ctera.com>
2010-04-25 14:29:43 +0300

    patch print_journal_err.patch

--- e2fsck/journal.c
+++ e2fsck/journal.c
@@ -836,6 +836,29 @@ static errcode_t recover_ext3_journal(e2fsck_t ctx)
 
 
 	if (journal->j_superblock->s_errno) {
+		/* journal message buffer at journal super block + 1K */
+		char *buf = ((char *) journal->j_superblock) +
+			SUPERBLOCK_OFFSET;
+		unsigned offset = 0;
+		int len = ctx->fs->blocksize - 2*SUPERBLOCK_OFFSET;
+#define MSGLEN 256
+
+		if (len > 0 && *buf) {
+			/* print messages in buffer */
+			puts("Error messages recorded in journal:");
+			while (offset < len && buf[offset]) {
+				printf(buf+offset);
+				offset += MSGLEN;
+			}
+			/* write journal message buffer to super block + 2K */
+			io_channel_set_blksize(ctx->fs->io, SUPERBLOCK_OFFSET);
+			retval = io_channel_write_blk(ctx->fs->io, 2, 2, buf);
+			io_channel_set_blksize(ctx->fs->io, ctx->fs->blocksize);
+			/* clear journal message buffer */
+			memset(buf, 0, len);
+			puts("End of journal message buffer.");
+		}
+
 		ctx->fs->super->s_state |= EXT2_ERROR_FS;
 		ext2fs_mark_super_dirty(ctx->fs);
 		journal->j_superblock->s_errno = 0;
--- version.h
+++ version.h
@@ -7,5 +7,5 @@
  * redistributed under the GNU Public License.
  */
 
-#define E2FSPROGS_VERSION "1.41.9-next3-1.0.9"
-#define E2FSPROGS_DATE "21-Apr-2010"
+#define E2FSPROGS_VERSION "1.41.9-next3-1.0.10-WIP"
+#define E2FSPROGS_DATE "25-Apr-2010"
