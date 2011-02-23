32673a9e7e4855e8fdf83c53f210a7040b00f328
Amir Goldstein <amir73il@users.sf.net>
2011-02-23 22:24:06 +0200

    fix missing has_snapshot feature on mkfs.next3

--- version.h
+++ version.h
@@ -7,5 +7,5 @@
  * file may be redistributed under the GNU Public License v2.
  */
 
-#define E2FSPROGS_VERSION "1.41.14-next3-1.0.13-1"
-#define E2FSPROGS_DATE "22-Feb-2011"
+#define E2FSPROGS_VERSION "1.41.14-next3-1.0.13-2"
+#define E2FSPROGS_DATE "23-Feb-2011"
