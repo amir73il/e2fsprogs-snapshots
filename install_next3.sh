447b5b856dffe98d92275f14db2e6365dbb70acb
Amir Goldstein <amir73il@users.sf.net>
2011-03-12 15:13:35 +0200

    add install scripts for next3/next4 utils
    before running next4 script install_next4.sh should
    be run to build and install the .next4 utils.
    
    the .next3 utils are used for backward compatibility with next3.
    
    I added the next4 script to this repository, so the install scripts
    will install it as well.
    The same script is used for both next4 and next3.

---
+++ install_next3.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Install next3 patched e2fsprogs
+
+# build e2fsprogs from e2fsprogs-snapshots git
+#./configure
+make
+# install progs with .next3 suffix
+install -T e2fsck/e2fsck /sbin/fsck.next3
+install -T misc/mke2fs /sbin/mkfs.next3
+install -T misc/tune2fs /sbin/tunefs.next3
+install -T misc/dumpe2fs /sbin/dumpfs.next3
+install -T misc/lsattr /sbin/lsattr.next3
+install -T misc/chattr /sbin/chattr.next3
+install -T resize/resize2fs /sbin/resize.next3
+# next4 script is generic and chooses the FS type by it's own name
+install -T next4 /sbin/next3
+
---
+++ install_next4.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# Install next4 patched e2fsprogs
+
+# build e2fsprogs from e2fsprogs-snapshots git
+#./configure
+make
+# install progs with .next4 suffix
+install -T e2fsck/e2fsck /sbin/fsck.next4
+install -T misc/mke2fs /sbin/mkfs.next4
+install -T misc/tune2fs /sbin/tunefs.next4
+install -T misc/dumpe2fs /sbin/dumpfs.next4
+install -T misc/lsattr /sbin/lsattr.next4
+install -T misc/chattr /sbin/chattr.next4
+install -T misc/lsattr /sbin/lssnap
+install -T misc/chattr /sbin/chsnap
+install -T resize/resize2fs /sbin/resize.next4
+install -T next4 /sbin/next4
